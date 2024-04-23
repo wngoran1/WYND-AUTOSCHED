d90ba0231abcf0f2c21416c9f9d9214e11f02843
wngoran1 <123908706+wngoran1@users.noreply.github.com>
2024-04-23 19:30:34 -0400

    end

--- Automatic_Scheduling_App/obj/Debug/net8.0/Automatic_Scheduling_App.AssemblyInfo.cs
+++ Automatic_Scheduling_App/obj/Debug/net8.0/Automatic_Scheduling_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Automatic_Scheduling_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cada7077d29342406cb9d1e83b1ffa8f302b9a6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c52891f95fbebdf7ee4676a541a7f82fef87466b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Automatic_Scheduling_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Automatic_Scheduling_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
